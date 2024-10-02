87f540f355cd82d688dfd91029f23878768bf80f
LeeSeungHun-35 <ish0542@naver.com>
2024-10-02 11:42:53 +0900

    dto설정

---
+++ src/main/java/com/PJ/kfs/dto/userdto.java
@@ -0,0 +1,13 @@
+package com.PJ.kfs.dto;
+
+public class userdto {
+    Long id;
+    String name;
+    String pwd;
+    String address;
+    String email;
+    Long  iq;
+    Long eq;
+    Long mt;
+    Long rk;
+}
